c773c138e35eb81a7cb12b74e4ffb12a4a0943c8
Briac Le Cam <briac@MacBook-Pro-de-Briac.local>
2024-05-01 22:06:16 +0200

    second commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,15 +3,21 @@ import { Link } from "react-router-dom";
 //style
 import "../styles/hero.scss";
 
+//images
+import SearchIcon from "../images/search-icon.svg";
+
 const Hero = () => {
   return (
     <section className="hero-container">
       <span>Find vegan venues nearby</span>
-      <input
-        type="text"
-        name="search-input"
-        placeholder=" City, restaurant, shop..."
-      />
+      <div className="search-bar">
+        <input
+          type="text"
+          name="search-input"
+          placeholder="City, restaurant, shop..."
+        />
+        <img src={SearchIcon} alt="loupe" />
+      </div>
     </section>
   );
 };
